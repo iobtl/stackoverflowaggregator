af3b6743becfcde725214194dd35ceace03bdd25
iobtl <tedmundhtl@gmail.com>
2020-08-15 16:25:12 +0800

    Added 'clear' button to reset filters

--- script.js
+++ script.js
@@ -72,8 +72,8 @@ const cleanData = (data) => {
     data = data.flatMap((entry) => entry.split(";")).filter((d) => d != "NA");
 
     const dataCount = new Object();
-    data.forEach(d => {
-      dataCount[d] = (dataCount[d] || 0) + 1;
+    data.forEach((d) => {
+        dataCount[d] = (dataCount[d] || 0) + 1;
     });
 
     const sortedCount = {};
@@ -93,6 +93,8 @@ const updateChartData = (metric) => {
 
     if (country !== "") {
         data = ds.filter((d) => d["Country"] == country);
+    } else {
+        data = ds;
     }
 
     data = cleanData(data.map((d) => d[metric]));
@@ -121,7 +123,7 @@ const addCountries = (data) => {
     });
 };
 
-// TODO: Add 'global' option
+// Performing remapping of the dataset to the chart depending on the filter selected
 const updateCountryFilter = (country) => {
     const metric = document.getElementById("surveyMetrics").value;
     const title = titles[metrics.indexOf(metric)];
@@ -133,6 +135,18 @@ const updateCountryFilter = (country) => {
     chart.destroy();
 
     chart = makeChart(newDataCount, title);
+};
+
+// Resetting filter to default i.e. showing data from all countries
+const clearCountryFilter = () => {
+    document.getElementById("countryFilter").selectedIndex = 0;
+    const metric = document.getElementById("surveyMetrics").value;
+    const data = cleanData(ds.map((d) => d[metric]));
+    const title = titles[metrics.indexOf(metric)];
+
+    chart.destroy();
+
+    chart = makeChart(data, title);
 };
 
 var ds;
