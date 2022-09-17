24482e807421754e6284f8bca0a529362c4b5048
iobtl <tedmundhtl@gmail.com>
2022-09-17 22:35:26 +0800

    feat: add support for 2022 dataset

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const metrics = [
-    "LanguageDesireNextYear",
-    "LanguageWorkedWith",
-    "MiscTechDesireNextYear",
-    "MiscTechWorkedWith",
-    "WebframeDesireNextYear",
-    "WebframeWorkedWith",
-    "DevType",
-];
+const metrics = {
+    2020: {
+        "DesiredLanguage": "LanguageDesireNextYear",
+        "WorkedLanguage": "LanguageWorkedWith",
+        "DesiredTech": "MiscTechDesireNextYear",
+        "WorkedTech": "MiscTechWorkedWith",
+        "DesiredWebframe": "WebframeDesireNextYear",
+        "WorkedWebframe": "WebframeWorkedWith",
+        "DevType": "DevType"
+    },
+    2022: {
+        "DesiredLanguage": "LanguageWantToWorkWith",
+        "WorkedLanguage": "LanguageHaveWorkedWith",
+        "DesiredTech": "MiscTechHaveWorkedWith",
+        "WorkedTech": "MiscTechWantToWorkWith",
+        "DesiredWebframe": "WebframeHaveWorkedWith",
+        "WorkedWebframe": "WebframeWantToWorkWith",
+        "DevType": "DevType"
+    }
+}
+
+const options = {
+    "DesiredLanguage": "Desired Languages",
+    "WorkedLanguage": "Languages Worked With",
+    "DesiredTech": "Desired Technologies",
+    "WorkedTech": "Technologies Worked With",
+    "DesiredWebframe": "Desired Web Framework",
+    "WorkedWebframe": "Web Framework worked with",
+    "DevType": "Type of Developer"
+}
 
 const titles = [
     "Desired language to work with next year",
@@ -26,7 +47,7 @@ const makeChart = (data, title) => {
             datasets: [
                 {
                     data: Object.values(data),
-                    backgroundColor: Object.keys(data).map((d) =>
+                    backgroundColor: Object.keys(data).map(() =>
                         getRandomColor()
                     ),
                 },
@@ -68,11 +89,11 @@ const getRandomColor = () => {
     return color;
 };
 
-const cleanData = (data) => {
-    data = data.flatMap((entry) => entry.split(";")).filter((d) => d != "NA");
+const cleanData = data => {
+    data = data.flatMap(entry => entry.split(";")).filter(d => d != "NA");
 
     const dataCount = new Object();
-    data.forEach((d) => {
+    data.forEach(d => {
         dataCount[d] = (dataCount[d] || 0) + 1;
     });
 
@@ -87,29 +108,29 @@ const cleanData = (data) => {
 };
 
 // Function for performing chart updates with corresponding data to prompt a change in the view
-const updateChartData = (metric) => {
+const updateChartData = metric => {
     var data;
     const country = document.getElementById("countryFilter").value;
 
     if (country !== "") {
-        data = ds.filter((d) => d["Country"] == country);
+        data = ds.filter(d => d["Country"] == country);
     } else {
         data = ds;
     }
 
-    data = cleanData(data.map((d) => d[metric]));
-    const title = titles[metrics.indexOf(metric)];
+    const currentMetrics = metrics[currentYear];
+    data = cleanData(data.map(d => d[currentMetrics[metric]]));
+    const title = titles[Object.keys(currentMetrics).indexOf(metric)];
 
     chart.destroy();
-
     chart = makeChart(data, title);
 };
 
 // Initialize list of countries from dataset
-const addCountries = (data) => {
-    var countries = new Set(data.map((d) => d["Country"]));
+const addCountries = data => {
+    var countries = new Set(data.map(d => d["Country"]));
     countries = new Set(
-        [...countries].sort().filter((country) => country !== "NA")
+        [...countries].sort().filter(country => country !== "NA")
     );
 
     const countrySelectTag = document.getElementById("countryFilter");
@@ -124,16 +145,17 @@ const addCountries = (data) => {
 };
 
 // Performing remapping of the dataset to the chart depending on the filter selected
-const updateCountryFilter = (country) => {
+const updateCountryFilter = country => {
     const metric = document.getElementById("surveyMetrics").value;
-    const title = titles[metrics.indexOf(metric)];
+    const currentMetrics = metrics[currentYear];
+
+    const title = titles[Object.keys(currentMetrics).indexOf(metric)];
     const newData = ds
-        .filter((d) => d["Country"] == country)
-        .map((d) => d[metric]);
+        .filter(d => d["Country"] == country)
+        .map(d => d[currentMetrics[metric]]);
     const newDataCount = cleanData(newData);
 
     chart.destroy();
-
     chart = makeChart(newDataCount, title);
 };
 
@@ -141,28 +163,85 @@ const updateCountryFilter = (country) => {
 const clearCountryFilter = () => {
     document.getElementById("countryFilter").selectedIndex = 0;
     const metric = document.getElementById("surveyMetrics").value;
-    const data = cleanData(ds.map((d) => d[metric]));
-    const title = titles[metrics.indexOf(metric)];
+    const currentMetrics = metrics[currentYear];
+
+    const data = cleanData(ds.map(d => d[currentMetrics[metric]]));
+    const title = titles[Object.keys(currentMetrics).indexOf(metric)];
 
     chart.destroy();
-
     chart = makeChart(data, title);
 };
 
+// Add survey years for which the datasets have been added
+const initYears = () => {
+    const yearTag = document.getElementById("yearFilter");
+
+    Object.keys(metrics).forEach(k => {
+        const option = document.createElement("option");
+        option.value = k;
+        option.text = k;
+        if (k == currentYear) {
+            option.selected = true;
+        }
+
+        yearTag.appendChild(option);
+    })
+}
+
+// Replaces current dataset with new one based on year selected
+const updateYearData = year => {
+    if (year !== currentYear) {
+        currentYear = year;
+
+        d3.csv(`survey_results_public_${year}.csv`).then(csv => {
+            ds = csv;
+
+            const metric = document.getElementById("surveyMetrics").value;
+            const currentMetrics = metrics[currentYear];
+
+            const data = cleanData(ds.map(d => d[currentMetrics[metric]]));
+            const title = titles[Object.keys(currentMetrics).indexOf(metric)];
+
+            chart.destroy();
+            chart = makeChart(data, title);
+        })
+    }
+}
+
+const initOptions = () => {
+    const metricsTag = document.getElementById("surveyMetrics");
+
+    var selectedAny = false;
+    Object.entries(options).forEach(([metric, description]) => {
+        const option = document.createElement("option");
+        option.value = metric;
+        option.text = description;
+        if (!selectedAny) {
+            option.selected = true;
+            selectedAny = true;
+        }
+
+        metricsTag.appendChild(option);
+    })
+}
+
 var ds;
 var chart;
+var currentYear = 2022;
 
-d3.csv("survey_results_public.csv").then((data) => {
-    ds = data;
+d3.csv(`survey_results_public_${currentYear}.csv`).then(csv => {
+    ds = csv;
 
+    initOptions();
     // Initializing chart with desired languages metric each reboot
-    // IDEA: progress bar??
     const metric = document.getElementById("surveyMetrics").value;
-    const dataCol = data.map((d) => d[metric]);
-    const dataCount = cleanData(dataCol);
-    const title = titles[metrics.indexOf(metric)];
+    const currentMetrics = metrics[currentYear];
 
-    chart = makeChart(dataCount, title);
+    const data = cleanData(ds.map(d => d[currentMetrics[metric]]));
+    const title = titles[Object.keys(currentMetrics).indexOf(metric)];
 
-    addCountries(data);
+    chart = makeChart(data, title);
+
+    addCountries(ds);
+    initYears();
 });
